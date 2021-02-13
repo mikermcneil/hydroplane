8fdaabe0e4387e024608bd396f45721e0d437225
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-02-13 17:22:42 -0600

    don't show home page because it's not relevant

--- config/routes.js
+++ config/routes.js
@@ -13,7 +13,8 @@ module.exports.routes = {
   //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
-  'GET /':                   { action: 'view-homepage-or-redirect' },
+  // 'GET /':                   { action: 'view-homepage-or-redirect' },
+  'GET /':                   '/welcome',
   'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
 
   // 'GET /faq':                { action:   'view-faq' },
