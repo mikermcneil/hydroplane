359fc4becdb7de7a9847d13d22a0dca012eb7807
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-02-13 15:35:19 -0600

    made it save

--- config/routes.js
+++ config/routes.js
@@ -62,4 +62,6 @@ module.exports.routes = {
   'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
   'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
 
+  'PUT   /api/v1/platform/routes':                       { action: 'platform/update-routes' },
+
 };
