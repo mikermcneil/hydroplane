d77b89c6073b6f71e234f8d067ed9217dbafa51d
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-02-13 17:21:25 -0600

    to avoid confusion, keep it looking like it comes out of the box

--- config/env/production.js
+++ config/env/production.js
@@ -70,7 +70,7 @@ module.exports = {
       * https://sailsjs.com/config/datastores                                     *
       *                                                                           *
       ****************************************************************************/
-      ssl: false,
+      // ssl: true,
 
     },
 
